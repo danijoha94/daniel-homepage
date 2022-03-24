966d7048536e28df6910c5d04682c01b98766a5f
Daniel Johansen <daniel@Daniels-MacBook-Pro.local>
2022-03-24 22:02:17 +0100

    Added header

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <h1>
           Daniel Johansen
+        </h1>
+        <p>
+          Kommer snart
         </p>
       </header>
     </div>
