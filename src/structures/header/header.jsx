1ad26509cebd51b85083ac8793887b5b9448c94f
danijoha <daniel@Daniels-MacBook-Pro.local>
2022-04-27 18:43:22 +0200

    Max width 1200px on conent and mobile padding

--- src/structures/header/header.jsx
+++ src/structures/header/header.jsx
@@ -1,51 +1,52 @@
 import "./header.css";
 export default function Header() {
-
   return (
     <section className='header'>
-      <div className="header__blur">
-        <nav className='header__nav'>
-          <ul className='header__nav-list'>
-            <li>
-              <a href='#'>
-                <h2>Daniel Johansen</h2>
-              </a>
-            </li>
-            <li>
-              <a href='#'>Prosjekter</a>
-            </li>
-            <li>
-              <a href='#'>Om meg</a>
-            </li>
-            <li>
-              <a href='#contact'>Kontakt</a>
-            </li>
-            <li>
-              <a className='header__some-icon' href='#'>
-                <span class='fab fa-facebook-square'></span>
-                <span className='sr-only'>facebook</span>
-              </a>
-            </li>
-            <li>
-              <a className='header__some-icon' href='#'>
-                <span class='fab fa-linkedin'></span>
-                <span className='sr-only'>linkedin</span>
-              </a>
-            </li>
-            <li>
-              <a className='header__cv' href='#'>
-                CV
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <p className='header__intro'>Hei, Mitt navn er</p>
-        <span className='header__name'>Daniel Johansen</span>
-        <h2 className='header__occupation'>Systemutvikler</h2>
-        <p className='header__intro-text'>
-          Jeg er frontend-utvikler for NRK og jobber med nettsiden yr.no. For
-          øyeblikket prøver jeg meg på <a>"CSS only"-prosjekter</a>
-        </p>
+      <div className='header__blur'>
+        <div className='header__content-container'>
+          <nav className='header__nav'>
+            <ul className='header__nav-list'>
+              <li>
+                <a href='#'>
+                  <h2>Daniel Johansen</h2>
+                </a>
+              </li>
+              <li>
+                <a href='#'>Prosjekter</a>
+              </li>
+              <li>
+                <a href='#'>Om meg</a>
+              </li>
+              <li>
+                <a href='#contact'>Kontakt</a>
+              </li>
+              <li>
+                <a className='header__some-icon' href='#'>
+                  <span class='fab fa-facebook-square'></span>
+                  <span className='sr-only'>facebook</span>
+                </a>
+              </li>
+              <li>
+                <a className='header__some-icon' href='#'>
+                  <span class='fab fa-linkedin'></span>
+                  <span className='sr-only'>linkedin</span>
+                </a>
+              </li>
+              <li>
+                <a className='header__cv' href='#'>
+                  CV
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <p className='header__intro'>Hei, Mitt navn er</p>
+          <span className='header__name'>Daniel Johansen</span>
+          <h2 className='header__occupation'>Systemutvikler</h2>
+          <p className='header__intro-text'>
+            Jeg er frontend-utvikler for NRK og jobber med nettsiden yr.no. For
+            øyeblikket prøver jeg meg på <a>"CSS only"-prosjekter</a>
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/structures/projects/projects.jsx
+++ src/structures/projects/projects.jsx
@@ -3,55 +3,57 @@ import "./projects.css";
 export default function Projects() {
   return (
     <section className='projects'>
-      <h2 className='projects__header'>Mine prosjekter</h2>
-      <article className='projects__project'>
-        <img src='/assets/article-1.jpg'></img>
-        <div className="projects__project-content">
-          <p className='projects__project-title'>Lorem Ipsum dorem</p>
-          <h3 className='projects__project-header'>Dorem foo</h3>
-          <p className='projects__project-description'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-      </article>
-      <article className='projects__project reverted'>
-        <div className="projects__project-content">
-          <p className='projects__project-title'>Lorem Ipsum dorem</p>
-          <h3 className='projects__project-header'>Dorem foo</h3>
-          <p className='projects__project-description'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-        <img src='/assets/article-2.jpg'></img>
-      </article>
-      <article className='projects__project'>
-        <img src='/assets/article-1.jpg'></img>
-        <div className="projects__project-content">
-          <p className='projects__project-title'>Lorem Ipsum dorem</p>
-          <h3 className='projects__project-header'>Dorem foo</h3>
-          <p className='projects__project-description'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-      </article>
+      <div className="projects__content-container">
+        <h2 className='projects__header'>Mine prosjekter</h2>
+        <article className='projects__project'>
+          <img src='/assets/article-1.jpg'></img>
+          <div className='projects__project-content'>
+            <p className='projects__project-title'>Lorem Ipsum dorem</p>
+            <h3 className='projects__project-header'>Dorem foo</h3>
+            <p className='projects__project-description'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </article>
+        <article className='projects__project reverted'>
+          <div className='projects__project-content'>
+            <p className='projects__project-title'>Lorem Ipsum dorem</p>
+            <h3 className='projects__project-header'>Dorem foo</h3>
+            <p className='projects__project-description'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <img src='/assets/article-2.jpg'></img>
+        </article>
+        <article className='projects__project'>
+          <img src='/assets/article-1.jpg'></img>
+          <div className='projects__project-content'>
+            <p className='projects__project-title'>Lorem Ipsum dorem</p>
+            <h3 className='projects__project-header'>Dorem foo</h3>
+            <p className='projects__project-description'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </article>
+      </div>
     </section>
   );
 }
